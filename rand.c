de7ebdea5fe5910ed6953d06f06054f70c032d4a
Giorgos <geo.giatrakos@gmail.com>
2017-03-18 22:30:35 +0200

    Add option to not print results for each iteration ,updated readme,added a bit of documentation

--- rand.c
+++ rand.c
@@ -5,19 +5,24 @@
 	void runSimulation(long goal, long startPoint, int visible, int mod);
 
 	int main(int argc, char *argv[]) {
+		int printresults ;
+		printresults = (argc > 3 ? 0 : 1) ; /*if a third option is chosen then it doesn't prints the results  */
 		srand(time(NULL));
-		runSimulation(atoi(argv[2]), atoi(argv[1]), 1, 1);
-		
+		runSimulation(atoi(argv[2]), atoi(argv[1]), printresults, 1);
+
 		return 0;
 	}
+
+	/*This programm uses the equation to make */
+	/* a' = a (operator) b */
 
 	void runSimulation(long goal, long startPoint, int visible, int mod) {
 		long count = 0;
 
 		while(startPoint != goal) {
 			count++;
-			long oper = rand() % (4 + mod);
-			long range = (rand() % 100) + 1;
+			long oper = rand() % (4 + mod);			/* Gets operator randomly		*/
+			long range = (rand() % 100) + 1;		/* Gets b out of random range   */
 			char voper = 'E';
 
 			switch(oper) {
@@ -26,7 +31,7 @@
 					voper = '+';
 					break;
 				case 1:
-					startPoint = startPoint - range; 
+					startPoint = startPoint - range;
 					voper = '-';
 					break;
 				case 2:
@@ -42,7 +47,7 @@
 					voper = '%';
 					break;
 			}
-			
+
 			if(visible) {
 				printf("%c %ld = %ld\n", voper, range, startPoint);
 			}
